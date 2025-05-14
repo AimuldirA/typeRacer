773ba5697b5ffe015291f1b6498df6c5de8a7a29
AimuldirA <aimoshaibek03@gmail.com>
2025-05-15 06:39:52 +0800

    update socket

--- typeRacer/backend/src/server.ts
+++ typeRacer/backend/src/server.ts
@@ -17,9 +17,20 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 // Middleware
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://type-racer-uggc.vercel.app'
+];
+
 app.use(cors({
-  origin: 'https://type-racer-uggc.vercel.app',
-  credentials: true,
+  origin: (origin, callback) => {
+    // origin undefined байж болно (postman, server-to-server)
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    callback(new Error(`CORS blocked for origin: ${origin}`));
+  },
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
